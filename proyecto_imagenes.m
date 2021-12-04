68123352d89e83f15e4e1d4aace1048fc2c881b6
Hugo <hugo.arenas@usach.cl>
2021-12-04 18:26:21 -0300

    Nose que esta pasando

--- proyecto_imagenes.m
+++ proyecto_imagenes.m
@@ -1,29 +1,18 @@
-%i=imread('datos prueba\RGB-Images\Placa1-imagen1.jpg');
-i=imread('celulas_cancer.png');
-%i=imread('cancer\162-CPDAI-13-IDA-3-30.jpg');
-ir = i(:,:,1);
-ig = i(:,:,2);
-ib = i(:,:,3);
-icolor = 255 - (ib - fix(ir/2) - fix(ig/2));
-ig = rgb2gray(i);
-ig2 = 255 - ig;
-%se = strel('disk',3);
-%ie = imerode(ig,se);
-icon = adapthisteq(ig,'clipLimit',0.02,'Distribution','rayleigh');
-icon2 = adapthisteq(ig2,'clipLimit',0.02,'Distribution','rayleigh');
+i=imread('datosPrueba\imgs\3+_16_0_200_400_600.png');
+i2=imread('datosPrueba\masks\3+_16_mask_0_200_400_600.png');
+ired = i(:,:,1);
+igreen = i(:,:,2);
+iblue = i(:,:,3);
+icolor = (255 - iblue) - ired/2 - igreen/2;
+icon = adapthisteq(icolor,'clipLimit',0.02,'Distribution','rayleigh');
+%icon = imlocalbrighten(icon,0.2);
 umbral=graythresh(icon);
 ib=imbinarize(icon,umbral);
-umbral2=graythresh(icon2);
-ib2=imbinarize(icon2,umbral2);
-ibf = (1 - ib)- ib2;
-%se = strel('disk',4);
-%ie = imerode(ib,se);
-ib5 = edge(icon,'Sobel');
-%se = strel('disk',4);
-%ie = imdilate(ib5,se);
-%ie = imerode(ie,se);
-%ie = bwmorph(ib5,'bridge');
-%ie = ib - ie;
+
+%umbral=adaptthresh(icon,'NeighborhoodSize',[5 5]);
+%ib=imbinarize(icon,umbral);
+se = strel('disk',4);
+ie = imerode(ib,se);
 
 %iGT1 = imread('datos prueba\Acrosome-Masks\Placa1-imagen1.jpg');
 %iGT2 = imread('datos prueba\Head-Masks\Placa1-imagen1.jpg');
@@ -33,8 +22,9 @@ ib5 = edge(icon,'Sobel');
 %iSeg2 = imbinarize(ig,umbral);
 %sim=dice(iSeg,iGT1);
 
-figure, 
-subplot(1,2,1), imshow(ib), title('original'); 
-titulo=['umbral Otsu = ' int2str(umbral*255)];
-subplot(1,2,2), imshow(ib2), title(titulo);
+figure,
+subplot(1,3,1), imshow(i), title('original');
+subplot(1,3,2), imshow(ie), title('procesada'); 
+titulo=['mascara original'];
+subplot(1,3,3), imshow(i2), title(titulo);
 
